dcd1c40622a41a3ed29257859ca27f9280aef667
mpstaud <98075113+mpstaud@users.noreply.github.com>
2024-04-15 15:43:30 -0500

    Update basicCircuitCalc.py

--- Python/basicCircuitCalc.py
+++ Python/basicCircuitCalc.py
@@ -1,5 +1,5 @@
 # Ohm's law V = IR
-
+resistors = []
 def voltage(i, r):
   volt = i * r
   return volt
@@ -16,6 +16,12 @@ def main():
   circuit_type = input('Press S for Series, P for Parallel, C for a combination of the 2: ')
   if circuit_type == 'S':
     res_num_series = input('How many resistors do you have? ')
+    for i in range(0, res_num_series):
+      resistance = input("Enter the resistance in Ohms for this resistor")
+      resistors.append(resistance)
+      
+
+
     
     
   
