71f8674b4c823b93c7b1cc9de5c4ce86780f444e
Matthew Staudacher <98075113+mpstaud@users.noreply.github.com>
2024-07-09 10:00:38 -0500

    Update main.js

--- JavaScript/gpacalculator/main.js
+++ JavaScript/gpacalculator/main.js
@@ -1,2 +1,38 @@
 
 let cgpa;
+
+function qualityPoints(lgrade, credithrs){
+  let qpoints;
+  if (lgrade == "A"){
+    qpoints = credithrs * 4;  
+  }
+  else if (lgrade == "B"){
+    qpoints = credithrs * 3;
+  }
+  else if (lgrade == "C"){
+    qpoints = credithrs * 2;
+  }
+  else if (lgrade == "D"){
+    qpoints = credithrs;
+  }
+  else {
+    qpoints = 0;
+  }
+  return qpoints;
+}
+
+function getInputValue(){
+  let grade1 = document.getElementById("input1").value;
+  let grade2 = document.getElementById("input2").value;
+  let grade3 = document.getElementById("input3").value;
+  let grade4 = document.getElementById("input4").value;
+  let grade5 = document.getElementById("input5").value;
+  
+  const letterGrades = new Array(grade1,grade2,grade3,grade4,grade5);
+  let length = letterGrades.length;
+  for (let i=0; i < length; i++){
+    
+  }
+
+  return;
+}
